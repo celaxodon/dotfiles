141b8b27b71288eea84146cd77fa0d8c5542af63
Graham Leva <g.ravel66@gmail.com>
2015-08-31 21:38:20 -0500

    Add startup script for tmux

--- .tmux/startup.sh
+++ .tmux/startup.sh
@@ -1,14 +1,34 @@
 #!/bin/bash
 
 # A script for interactive tmux on startup
+# Parts of it from github.com/nicknisi/dotfiles/bin/tm
 
+# Exit if already inside a tmux session
+[[ $TMUX == "" ]] || exit 0;
 
 # Choose session to attach to, if one exists:
-# Show sessions:
-#tmux list-sessions
 
-# If you want a new session, or don't have any existing:
-#tmux new-session
+# Get number of active sessions:
+session_nums=$(tmux ls | wc -l)
 
-# Prompt for session name:
-input = read
+# Get list of sessions:
+session_list=($(tmux ls -F "#S") "New Session")
+
+PS3="Select a session to attach to (1-${session_nums}): "
+
+echo "Available sessions:"
+echo "-------------------"
+echo
+select i in $(session_list); do
+    case $i in 
+            "New Session") 
+                    read -p "Enter a new session name: " session_name;
+                    tmux new -s "$session_name";
+                    # Case needs extra semicolons at ends of case statements; see help case
+                    break;;
+            *)
+                    tmux attach-session -t "$i"
+                    break;;
+    esac
+done
+exit 0
