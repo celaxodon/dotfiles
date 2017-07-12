69bdbd176e1ea2521efdcef6bf988fd6daee8874
Graham Leva <g.ravel66@gmail.com>
2017-07-11 23:29:20 -0500

    Update Linux branch with new dotfiles

---
+++ .iex.exs
@@ -0,0 +1,8 @@
+# Load another ".iex.exs file"
+#import_file "~/.iex.exs"
+
+# Print something before the shell starts
+#IO.puts "hello world"
+
+# Bind a variable that'll be accessible in the shell
+#value = 13
